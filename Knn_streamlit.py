74a07377401bfb0d08f6fc8dbc24de445aed8ce0
Dattaj <102431255+Dattaj@users.noreply.github.com>
2022-04-16 17:04:37 +0530

    Add files via upload

---
+++ Knn_streamlit.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 16 14:40:45 2022
+
+@author: DJO044
+"""
+
+import streamlit as st
+import pandas as pd
+import joblib
+from PIL import Image
+
+#Loading Our final trained Knn model
+model=open("Knn_Classifier.pkl","rb")
+knn_clf=joblib.load(model)
+
+st.title("Iris flower species Classification App")
+st.sidebar.title("Features")
+
+#loading images
+setosa = Image.open('setosa.jpg')
+versicolor = Image.open('versicolor.jpg')
+virginica = Image.open('virginica.jpg')
+
+#Intializing
+parameter_list=['Sepal Length (cm)', 'Sepal Width (cm)', 'Petal Lenght (cm)', 'Petal Width (cm)']
+parameter_input_values=[]
+parameter_default_values=['5.2', '3.2', '4.2', '1.2']
+values=[]
+
+
+#Display
+for parameter, parameter_df in zip(parameter_list, parameter_default_values):
+    values= st.sidebar.slider(label=parameter, key=parameter, value=float(parameter_df), min_value=0.0, max_value=8.0, step=0.1)
+    parameter_input_values.append(values)
+    
+input_variables=pd.DataFrame([parameter_input_values], columns=parameter_list, dtype=float)
+
+st.write(input_variables)
+
+prediction =0
+if st.button("Clck Here to Classify"):
+    prediction = knn_clf.predict(input_variables)
+    
+if prediction ==0:
+    st.image(setosa)
+elif prediction == 1:
+    st.image(versicolor)
+else:
+    st.image(virginica)
+    
+    
